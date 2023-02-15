1385fc003abc23435e19d52a420b173217fca54b
Rickey Smith <res0905@gmail.com>
2023-02-15 16:11:01 -0500

    installed basic framework

---
+++ src/components/dateSummary.jsx
@@ -0,0 +1,13 @@
+import GameDateDiv from "./gameDateDiv.jsx";
+
+function DateSummary(){
+    return(
+        <div id='date-summary'>
+            <h1 className='coda-font'>Calendar</h1>
+            <GameDateDiv />
+        </div>
+    )
+    
+}
+
+export default DateSummary;
---
+++ src/components/gameDateDiv.jsx
@@ -0,0 +1,22 @@
+
+function GameDateDiv(props){
+    return(
+        <div className='gameDateDiv' id='gameDateDiv-10'>
+            <div className='dateTimeInfo'>
+                <div className='date'>
+                    <div className='number'>29</div>
+                    <div className='month'>OCT</div>
+                </div>
+                <div className='time'>
+                    <div className='hour'>7PM</div>
+                </div>
+            </div>
+            <div className='vsInfo'>
+                NSH
+            </div>
+        </div>
+    )
+    
+}
+
+export default GameDateDiv;
